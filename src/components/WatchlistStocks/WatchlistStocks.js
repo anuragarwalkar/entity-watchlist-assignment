8881f692c01624e2167f4d379ca69b669c579ebd
Anurag Arwalkar <anurag.arwalkar@globant.com>
2023-02-14 17:40:02 +0530

    fixed data grid

--- src/components/WatchlistStocks/WatchlistStocks.js
+++ src/components/WatchlistStocks/WatchlistStocks.js
@@ -1,57 +1,23 @@
 import { Fragment} from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import ClearIcon from "@mui/icons-material/Clear";
-import { IconButton } from "@mui/material";
+import { Box } from "@mui/material";
 import useTableData from "./useTableData";
 import useStocklistWS from "./useStocklistWS";
+import { DataGrid } from '@mui/x-data-grid';
 
 function WatchlistStocks() {
-  const { convertToFixed, rows, removeStock } = useTableData();
+  const { rows, columns } = useTableData();
   useStocklistWS();
 
   return (
     <Fragment>
-      <TableContainer component={Paper} style={{ marginTop: "20px" }}>
-        <Table sx={{ minWidth: 550 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Company</TableCell>
-              <TableCell align="right">Market Ptice</TableCell>
-              <TableCell align="right">52W&nbsp;low</TableCell>
-              <TableCell align="right">52W&nbsp;high</TableCell>
-              <TableCell align="right"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.company}
-                </TableCell>
-                <TableCell align="right">
-                  ${convertToFixed(row.marketPrice)}
-                </TableCell>
-                <TableCell align="right">${convertToFixed(row.low)}</TableCell>
-                <TableCell align="right">${convertToFixed(row.high)}</TableCell>
-                <TableCell align="right">
-                  <IconButton onClick={() => removeStock(row)}>
-                    <ClearIcon className="cursor-pointer" />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Box sx={{ width: 500, height: 300, marginTop: "20px" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+        />
+      </Box>
     </Fragment>
   );
 }
--- src/components/WatchlistStocks/useTableData.js
+++ src/components/WatchlistStocks/useTableData.js
@@ -1,29 +1,81 @@
-import { removeWatchlistStocks, selectSelectedWatchlistStocks } from "@/slice/stocksSlice";
-import { useEffect, useState } from "react";
+import {
+  removeWatchlistStocks,
+  selectSelectedWatchlistStocks,
+} from "@/slice/stocksSlice";
+import { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ClearIcon from "@mui/icons-material/Clear";
+import { IconButton } from "@mui/material";
 
-function useTableData () {
+function useTableData() {
   const allSelectedSocks = useSelector(selectSelectedWatchlistStocks);
   const [rows, setRowsData] = useState([]);
   const dispatch = useDispatch();
 
-  const createData = ({price, name, fifty_two_week, ...rest}) =>  {
-    return { company: name, marketPrice: price, low: fifty_two_week.low, high: fifty_two_week.high, ...rest };
-  }
+  const createData = ({ price, name, fifty_two_week, ...rest }) => {
+    return {
+      company: name,
+      marketPrice: price,
+      low: fifty_two_week.low,
+      high: fifty_two_week.high,
+      ...rest,
+    };
+  };
 
   const removeStock = (row) => {
-    dispatch(removeWatchlistStocks(row))
-  }
+    dispatch(removeWatchlistStocks(row));
+  };
 
   useEffect(() => {
     setRowsData(allSelectedSocks.map(createData));
   }, [allSelectedSocks]);
 
   const convertToFixed = (strNumber) => {
-    return parseFloat(strNumber).toFixed(2)
-  }
+    return parseFloat(strNumber).toFixed(2);
+  };
 
-  return {convertToFixed, rows, removeStock}
+  const columns = [
+    {
+      field: "company",
+      headerName: "Company",
+      width: 120,
+    },
+    {
+      field: "marketPrice",
+      headerName: "Market Price",
+      renderCell: (params) => {
+        return <Fragment>${convertToFixed(params.row.marketPrice)}</Fragment>;
+      },
+    },
+    {
+      field: "low",
+      headerName: "52W low",
+      renderCell: (params) => {
+        return <Fragment>${convertToFixed(params.row.low)}</Fragment>;
+      },
+    },
+    {
+      field: "high",
+      headerName: "52W high",
+      renderCell: (params) => {
+        return <Fragment>${convertToFixed(params.row.high)}</Fragment>;
+      },
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 80,
+      renderCell: (params) => {
+        return (
+          <IconButton onClick={() => removeStock(params.row)}>
+            <ClearIcon className="cursor-pointer" />
+          </IconButton>
+        );
+      },
+    },
+  ];
+
+  return { convertToFixed, rows, removeStock, columns };
 }
 
 export default useTableData;
